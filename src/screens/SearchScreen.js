d8b881982edcb0d7791e7f078e318e37bd2f354e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-04-21 12:32:20 -0400

    Add search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,36 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const SearchScreen = () => {
     return (
         <View>
-            <Text>Search screen</Text>
+            <View style={styles.searchBarContainer}>
+                <Icon name="search" size={30} style={styles.searchBarIcon} />
+                <TextInput
+                    placeholder="enter search..."
+                    style={styles.searchBarTextInput}
+                />
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        backgroundColor: "white",
+        borderRadius: 10,
+        flexDirection: "row",
+        margin: 10,
+        padding: 10,
+        alignItems: "center",
+    },
+    searchBarIcon: {
+        marginRight: 10,
+    },
+    searchBarTextInput: {
+        fontSize: 18,
+    },
+});
 
 export default SearchScreen;
