8d1a071f4d362c95fb360e2f7b67c0ae618ce032
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-04-21 12:14:47 -0400

    Add search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
